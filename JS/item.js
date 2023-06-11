8980e7246200d5ec40b3b2f30c4d2921064eca31
anashamdallh <anashamdallah93@gmail.com>
2023-06-11 13:21:43 +0000

    feat: adding item.js file

--- JS/item.js
+++ JS/item.js
@@ -1,15 +1,15 @@
 "use strict"
 
-// // read the json Array From LS 
+// read the json Array From LS 
 let jsonArray = window.localStorage.getItem("user choice");
 
 
-// // convert the JSON to JS 
+// convert the JSON to JS 
 let objDummyArray = JSON.parse(jsonArray);
 
 
 //Render Data
-function render(e) {
+function render() {
 
     //Creating elements
     let drinkName = document.getElementById("cardTitle");
@@ -26,8 +26,6 @@ function render(e) {
         ingredientsElements.textContent = drinkIngredientsArray[i];
     }
 
-
-
     let drinkSteps = document.getElementById("renderSteps");
     let drinkStepsArray = objDummyArray.steps.split(",");
     for (let i = 0; i < drinkStepsArray.length; i++) {
@@ -36,23 +34,15 @@ function render(e) {
         stepsElements.textContent = drinkStepsArray[i];
     }
 
-    //let stepsElements = document.createElement('li');
-    //Assigning values to the elements
     drinkName.textContent = objDummyArray.name;
     drinkImage.textContent = objDummyArray.image;
-
-
-    // //     //Appending elements
-    // //     drinkIngredients.appendChild(drinkIngredientsList);
-    // //     drinkSteps.appendChild(drinkStepsList);
-
 
     //Styling
     drinkIngredients.style.position = "absolute";
     drinkIngredients.style.left = "57px"
     drinkIngredients.style.top = "160px"
     drinkIngredients.style.display = "block";
-    drinkIngredients.style.width = "154px";
+    drinkIngredients.style.width = "176px";
     drinkIngredients.style.textAlign = "left";
     drinkIngredients.style.fontWeight = "500";
     drinkIngredients.style.fontSize = "18px";
@@ -61,7 +51,7 @@ function render(e) {
     drinkSteps.style.left = "57px"
     drinkSteps.style.top = "160px"
     drinkSteps.style.display = "block";
-    drinkSteps.style.width = "154px";
+    drinkSteps.style.width = "176px";
     drinkSteps.style.textAlign = "left";
     drinkSteps.style.fontWeight = "500";
     drinkSteps.style.fontSize = "18px";
